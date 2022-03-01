31524285696c6e5e52d5a039225899c6a0fbe80e
“yornvickie” <yornvickie@gmail.com>
2022-02-28 21:42:31 -0600

    added JS Basics Exercise Folder

---
+++ script.js
@@ -0,0 +1,49 @@
+console.log(31);
+
+// Numbers: 1
+// Strings: "Hello"
+// Booleans
+// Null
+// Undefined
+
+/*
++
+-
+/
+*
+**
+%
+*/
+
+console.log(5 % 4); // 1
+console.log(1 % 5); // 1
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+console.log("Hawk" + "eye");
+
+console.log("Vickie".length + "Yorn".length);
+
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+
+console.log('\\');
+
+console.log(5 ** 4 -9);
+
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+m = 25;
+c = 4;
+ var u = m + c;
+ console.log(u);
